dd06513979bb70794fa46b9a1d59ddbf623f607c
Naoki Komiya <edsa9654@gmail.com>
2020-01-13 17:16:02 +0900

    add kaggle_settings.py

---
+++ kaggle_settings.py
@@ -0,0 +1,9 @@
+# %%
+!pip install --upgrade pip
+!pip install autopep8 japanize-matplotlib kaggle python-language-server pyls
+!pip install -U pandas
+
+# %%
+# !mkdir ~/.kaggle
+# !cp /root/dev/kaggle.json ~/.kaggle
+# !chmod 600 ~/.kaggle/kaggle.json
